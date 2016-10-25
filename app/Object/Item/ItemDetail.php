b273c33e2ccdd7d1213c01c901d3faa6468e663e
ravenncrows <waitingforthen@gmail.com>
2016-10-25 21:17:35 +0700

    expense v0.001.1 + config and vendor

---
+++ app/Object/Item/ItemDetail.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Object\CC;
+
+use App\CC\Loader;
+use App\Object\CC\CCDetail as detail;
+
+class ItemDetail extends detail
+{
+    public function checkPermission($request)
+    {
+        return true;
+    }
+
+    public function process($request)
+    {   
+        $objectName = $request->route('objectName');
+        $record = (int)$request->route('record');
+        $objectClass =  Loader::getObject($objectName);
+        $objectModel = $objectClass::find($record);
+        $label = $objectModel->getLabel();
+        $layout =$this->convertLayout($objectModel);
+        $data = $this->convertData($objectModel);
+              
+        return [
+                "objectname"=>$objectName,
+                "record"=>$record,
+                "label"=>$label,
+                "blocks"=>$layout,
+                "data"=>$data
+            ]; 
+    }
+
+    public function convertLayout($objectModel)
+    {
+    	$layout = [];
+    	$Object = $objectModel->getObject();
+    	$Blocks = $Object->getBlock()->orderby('sequence')->get();
+    	foreach ($Blocks as $Block) {
+			$Fields = $Block->getField()->orderby('sequence')->get();
+			$Block->fields = $Fields;
+    	}
+    	return $Blocks;
+    }
+
+    public function convertData($objectModel)
+    {
+        return $objectModel;
+    }
+}
